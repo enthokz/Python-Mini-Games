d28d71fd6ce0bd36e68f1c855fc77efbee975f35
fahrulrah <fahrul.rah@gmail.com>
2025-08-31 11:06:43 +0700

    Update Function and looping

--- Mini_Games_Bahlil_Bahasa.py
+++ Mini_Games_Bahlil_Bahasa.py
@@ -1,80 +1,94 @@
-# Mari kita membuat mini games!
-# Membuat permainan bahlil v.1.0
+# Membuat permainan bahlil v.1.1
+# {"Selamat Datang":^28}
+# Buat greeting awal permainan
+def greet():
+    print(f'''{'-'*72}
+|{"Selamat Datang":^70}|
+{'-'*72}\n
+Untuk memulai permainan kamu perlu memasukan nama playermu.''')
+    player_name=input('Nama Player = ')
+    print(f'\nHallo {player_name}! Saatnya kita memulai permaiann...')
+    return player_name
 
-# Membuat gretings/ welcome
-greeting=' SELAMAT DATANG'
-print('------------------------------------------------------------------------')
-print(f'|                           {greeting}                            |')
-print('------------------------------------------------------------------------')
-print('Mari kita sedikit bermain permainan')
-
-# Membuat input nama player
-player_name=input('Masukan Nama Player : ')
-
-# Membuat deskripsi permainan
-print(f'''\n
-------------------------------------------------------------------------
-Selamat Datang {player_name} di permainan ini!\n''')
-print('''Permainannya cukup mudah. Mari kita menangkap bahlil!
+# Buat deskripsi permainan
+def describe():
+    print('''\nPermainannya cukup mudah. Mari kita menangkap bahlil!
 Bahlil saat ini sedang bersembunyi di goa berikut:
-      |1|  |2|  |3|  |4|''')
+       _    _    _    _
+      |1|  |2|  |3|  |4|
+       ‾    ‾    ‾    ‾''')
+    
+# Buat function pertanyaan
+# Dapat berfungsi juga sebagai validasi jawaban apakah sesuai dengan list jawaban yang tersedia 
+def question(msg,choice):
+    while True:
+        try:
+            ans= int(input(msg))
+            if ans in choice:
+                return ans
+            else:
+                print(f'\nCek kembali jawabanmu ya, jawabannnya hanya diantara {choice}')
+        except ValueError:
+            print(f'\nMohon cek lagi jawabanmu ya! Jawaban yang diinputkan hanya diantara {choice}')
 
-# Membuat input jawaban permainan
-quetion=int(input('Menurutmu ada di goa nomor berapakah bahlil bersembunyi? '))
-quetion_answer=quetion
-while quetion_answer:
-    if quetion_answer not in[1,2,3,4]:
-        print('Cek kembali yuk jawabanmu! Goa yang menjadi persembunyian bahlil hanya 1 hingga 4')
-        quetionn=int(input('Menurutmu ada di goa nomor berapakah bahlil bersembunyi? '))
-        quetion_answer=quetionn
-    elif quetion_answer in[1,2,3,4]:
-        break
-print('\n')
-confirm=input(f'Apakah kamu yakin bahwa bahlil ada di goa {quetion_answer} ? (Y/N)')
-confirm_answer=confirm
 
-# Membuat Hasil 
+# Buat Jawaban dari permainan menggunakan random
 import random
-answer=random.randint(1,4)
+key=random.randint(1,4)
 
-#Menampilkan hasil dengan syarat user sudah yakin dengan jawaban ('Y')
-while confirm_answer:
-    if confirm_answer not in['Y','N']:
-        print('Maaf, jawaban konfirmasi yang diterima hanya jawaban Y dan N')
-        quetion=int(input('Coba lagi yuk! Menurutmu ada di goa nomor berapakah bahlil bersembunyi seharusnya? '))
-        while quetion:
-            if quetion not in[1,2,3,4]:
-                print('Cek kembali yuk jawabanmu! Goa yang menjadi persembunyian bahlih hanya 1 hingga 4')
-                quetionn=int(input('Menurutmu ada di goa nomor berapakah bahlil bersembunyi? '))
-                quetion=quetionn
-            elif quetion in[1,2,3,4]:
-                 break
-        confirmm=input(f'Apakah kamu yakin bahwa bahlil ada di goa {quetion} ? (Y/N)')
-        confirm_answer=confirmm
-
-    elif confirm_answer=='N':
-        quetion=int(input('Lalu menurutmu ada di goa nomor berapakah bahlil bersembunyi seharusnya? '))
-        while quetion:
-            if quetion not in[1,2,3,4]:
-                print('Cek kembali yuk jawabanmu! Goa yang menjadi persembunyian bahlih hanya 1 hingga 4')
-                quetionn=int(input('Menurutmu ada di goa nomor berapakah bahlil bersembunyi? '))
-                quetion=quetionn
-            elif quetion in[1,2,3,4]:
-                 break
-        confirmm=input(f'Apakah kamu yakin bahwa bahlil ada di goa {quetion} ? (Y/N)')
-        confirm_answer=confirmm
-
-    elif confirm_answer=='Y':
-        if quetion==answer:
-            print(f'''
-------------------------------------------------------------------------
-Yeay selamat {player_name}!! Kamu bisa menagkap bahlil !!
-Goa {quetion} seperti yang kamu jawab memang menjadi tempat persembunyian Bahlil.
-------------------------------------------------------------------------''')
+# Buat Skema Permainan
+def game():
+    guess=question('Menurutmu ada di goa nomor berapakah bahlil bersembunyi? ',[1,2,3,4])
+    confirm=question('Apakah kamu yakin dengan jawabanmu? (1=Iya 2=Tidak)',[1,2])
+    while confirm:
+        if confirm==1:
+            return guess
         else:
-            print(f'''
-------------------------------------------------------------------------
+            guess=question('\nMenurutmu ada di goa nomor berapakah bahlil bersembunyi? ',[1,2,3,4])
+            confirm=question(f'Apakah kamu yakin dengan jawabanmu ada di goa {guess}? (1=Iya 2=Tidak)',[1,2])
+
+
+# Buat hasil permainan
+
+def result():
+    if ply_guess==key:
+        print(f'''
+{'-'*72}
+Yeay selamat {player}!! Kamu bisa menagkap bahlil !!
+Goa {ply_guess} seperti yang kamu jawab memang menjadi tempat persembunyian Bahlil.
+{'-'*72}''')
+    else:
+        print(f'''
+{'-'*72}
 Sayang sekali bahlil belum bisa tertangkap.
-Kamu menjawab goa {quetion} padahal bahlil bersembunyi di goa {answer}
-------------------------------------------------------------------------''')
+Kamu menjawab goa {ply_guess} padahal bahlil bersembunyi di goa {key}
+{'-'*72}''')
+    play_again=question('Apakah kamu ingin bermain lagi ? (1=Ya 2=Tidak)',[1,2])
+    return play_again
+        
+
+# Step by Step permainan
+'''
+Terbagi menajdi empat bagian:
+1.  Menampilkan menu greeting sekaligus mendifinisikan nama player untuk digunakan 
+    di function selanjutnya
+2.  Menampilkan menu deskirpsi dari game serta petunjuk permainannya
+3.  Menampilkan menu permainan sekaligus mendefinisikan jawaban dari pemain untuk digunakan
+    di funtion hasil
+4.  Menampilkan menu hasil dari rangkaian game
+'''
+player= greet()             #1
+while True:                 # Buat Looping jika ingin mengulang permainan
+    describe()              #2
+    ply_guess=game()        #3
+    end=result()            #4
+    if end ==2:
+        print(f'''\n{'-'*72}
+{"Terima Kasih Sudah Bermain!!":>52}
+{"See Yaa!!":>42}
+{'-'*72}''')
         break
+    else:
+        print(f'''\n{'-'*72}
+{"NEW GAME":>40}
+{'-'*72}''')
