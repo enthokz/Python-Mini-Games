be39ad486734a182c9439e47c306af01bbe5604a
fahrulrah <fahrul.rah@gmail.com>
2025-11-26 00:23:02 +0700

    OOP added
    need some review again

--- Quezzy/coret_tapi.py
+++ Quezzy/coret_tapi.py
@@ -1,3 +1,6 @@
+import random
+import math
+import pandas as pd
 ## Dict Petanyaan
 data= {'index':[1,2,3,4,5,6,7,8,9,10],
        'pertanyaan':['Nama zat hijau daun?',
@@ -11,17 +14,126 @@ data= {'index':[1,2,3,4,5,6,7,8,9,10],
                      'Nama penyakit yang diakibatkan oleh Salmonella typhi?',
                      'Nama zat yang mengakibatkan otot lelah?'],
        'jawaban':['klorofil','animalia','lidah','trombosit','ligamen','trakea','amilase','alveolus','tipes','asam laktat']}
-import random
-import math
 
-## User Name
-def user():
-    put= input(f'''{'-'*56}
+
+## Buat Kelas Database player
+class player:
+    def __init__ (self, player):
+        self.player=player
+        self.daftar=[]
+        self.nilai=[]
+    def pemain(self,nama,amount):
+        self.nama=nama
+        self.amount=amount
+        if nama in self.daftar:
+            a= self.daftar.index(nama)
+            self.daftar[a]=nama
+            if self.nilai[a]<amount:
+                self.nilai[a]=amount
+        else:
+            self.daftar.append(nama)  
+            self.nilai.append(amount)  
+    def hapus(self, apus):
+        self.apus=apus
+        while True:
+            try:
+                b= self.daftar.index(apus)
+                c= self.nilai[b]
+                self.daftar.remove(self.daftar[b])
+                self.nilai.remove(c)
+                print(f'Data pencapaian {apus} berhasil dihapus!')
+                break
+            except:
+                print('Nama yang kamu masukan tidak ada!')
+                break
+    def board(self):
+        dic= {'Nama': self.daftar,
+              'Nilai': self.nilai}
+        lead= pd.DataFrame(dic)
+        leaderboard= lead.sort_values('Nilai',ascending=False).reset_index(drop=True)
+        if self.daftar==[]:
+            print(f'''\n===={'LEADERBOARD':^47}====
+Maaf, belum ada data pemain yang tersedia''')
+        else:
+            print(f'''\n===={'LEADERBOARD':^47}====
+{leaderboard.to_string(index=False)}''')
+    
+user= player('person')
+
+# Pencaharian
+def cari():
+    while True:
+        a=[]
+        b=[]
+        siapa= input(f'''\n===={'CARI CAPAIAN PEMAIN':^47}====
+Siapa nama user yang ingin Anda cari? ''').lower()
+        for i in user.daftar:
+            if i==siapa:
+                a.append(i)
+                b.append(user.nilai[user.daftar.index(i)])
+        if a==[]:
+            print('Nama tidak ditemukan')
+        else:
+            print(f"Pemain dengan nama {a[0]} memiliki nilai terbaik {b[0]}")
+        r=question(f'\nApakah anda ingin mencari yang lain? (YA/TIDAK) ',['Ya','Tidak'])
+        if r=='tidak':
+            break
+
+
+## Exeption handling for Question
+def question(msg,choice):
+    cho= [i.lower() for i in choice]
+    while True:
+        jwb= input(msg).lower()
+        if jwb in cho:
+            return jwb
+        else:
+            print(f'\nCek kembali jawabanmu! Hanya {choice} yang diperbolehkan!')
+
+## Header Greetings
+def greeting():
+    print(f'''{'-'*56}
 |{'Welcome to Quezzy':^54}|
-{'-'*56}
-Siapa Namamu? ''')
-    print(f'\nHai {put}, Tugas kamu adalah menjaawab pertanyaan berikut dengan tepat.\nMari kita mulaiii!')
+{'-'*56}''')
+    
+# ## User Name
+def pemain():
+    global put
+    put= input(f'''
+Siapa Nama player mu? ''')
+    print(f'Halo {put}, Bagaimana kabarmu hari ini? Semoga sehat selalu ya.')
 
+## Homepage
+def home():
+    while True:
+        q= question(f'''===={'HOME MENU':^47}====
+Pilih Menu di bawah ini:
+A. Mulai Main
+B. Cek Capaian Pemain
+C. Cek Leadarboard
+D. Hapus Capaian User
+E. Keluar 
+Jawab => ''',['A','B','C','D','E'])
+        if q=='a':
+            break
+        elif q=='b':
+            cari()
+        elif q=='e':
+            return 'exit'
+        elif q=='c':
+            user.board()
+            input(f'\nSilahkan ketik apapun untuk melanjutkan => ')
+        elif q=='d':
+            d=question(f'''\nn===={'HAPUS CAPAIAN USER':^47}====
+Apakah Kamu yakin ingin menghapus capaian user {put}? (YA/TIDAK) ''',['YA','TIDAK'])
+            if d=='ya':
+                user.hapus(put)
+                input(f'\nSilahkan ketik apapun untuk melanjutkan => ')
+# home()
+# user()
+
+
+# # Question
 num= list(range(0,10))
 # random.shuffle(num)
 selnum= random.sample(num,5)
@@ -29,17 +141,20 @@ numb=[]
 benar=[]
 salah=[]
 def main():
-    a=0
-    for i in selnum:
-        if i not in numb:
-            a+=1
-            af= input(f"\n{a}. {data['pertanyaan'][i]} ").lower()
-            if af!=data['jawaban'][i]:
-                salah.append(i)
-            else:
-                benar.append(i)
-            numb.append(i)
-
+    print(f'''\n===={'MULAI PERMAINAN':^47}====
+Tugas kamu adalah menjaawab pertanyaan berikut dengan tepat.\nMari kita mulaiii!''')
+    while True:
+        a=0
+        for i in selnum:
+            if i not in numb:
+                a+=1
+                af= input(f"\n{a}. {data['pertanyaan'][i]} ").lower()
+                if af!=data['jawaban'][i]:
+                    salah.append(i)
+                else:
+                    benar.append(i)
+                numb.append(i)
+        break
 
 ## Result
 def review():
@@ -58,73 +173,30 @@ def review():
         print(f'''|{' ':>21}Nilai Kamu: {nilai}{' ':<20}|
 |{'Belajar lagi aja dek!':^54}|''')
     print(f"{'-'*56}")
-    
-    
-## Exeption handling for Question
-def question(msg,choice):
-    cho= [i.lower() for i in choice]
-    while True:
-        jwb= input(msg).lower()
-        if jwb in cho:
-            return jwb
-        else:
-            print(f'\nCek kembali jawabanmu! Hanya {choice} yang diperbolehkan!')
+    user.pemain(put,nilai)
 
-## Running the Game
-# while True:
-#     user()
-#     main()
-#     review()
-#     ask= question('Apakah Anda ingin bermain lagi? (Y/N) ',['Y','N'])
-#     if ask=='n':
-#         print(f'''\n{'-'*56}
-# |{'Terima Kasih Sudah Bermain!':^54}|
-# {'-'*56}\n''')
-#         break
-#     else:
-#         numb.clear()
-#         benar.clear()
-#         salah.clear()
-#         print(f'\n=== {'NEW GAME':^47} ===\n')
+# print(user.daftar)
+# ## Running the Game
 
-
-
-## Trying Design OOP
-class player:
-    def __init__ (self, player):
-        self.player=player
-        self.daftar=[]
-        self.nilai=[]
-    def pemain(self,nama,amount):
-        self.nama=nama
-        self.amount=amount
-        if nama in self.daftar:
-            a= self.daftar.index(self.nama)
-            self.daftar[a]=nama
-            if self.nilai[a]<amount:
-                self.nilai[a]=amount
-        else:
-            self.daftar.append(nama)  
-            self.nilai.append(amount)  
-
-
-user= player('person')
-user.pemain('Dontol',50)
-user.pemain('Fontol',70)
-user.pemain('Afdhol',100)
-user.pemain('Fontol',50)
-
-def cari(siapa):
-    a=[]
-    b=[]
-    for i in user.daftar:
-        if i==siapa:
-            a.append(i)
-            b.append(user.nilai[user.daftar.index(i)])
-    if a==[]:
-        print('Nama tidak ditemukan')
+greeting()
+pemain()
+while True:
+    gmn=home()
+    if gmn=='exit':
+        print(f'''\n{'-'*56}
+|{'Terima Kasih Sudah Bermain!':^54}|
+{'-'*56}\n''')
+        break
+    main()
+    review()
+    ask= question('Apakah Anda ingin bermain lagi? (Y/N) ',['Y','N'])
+    if ask=='n':
+        print(f'''\n{'-'*56}
+|{'Terima Kasih Sudah Bermain!':^54}|
+{'-'*56}\n''')
+        break
     else:
-        print(a)
-        print(b)
-        
-cari('Fontol')
+        numb.clear()
+        benar.clear()
+        salah.clear()
+        print(f'\n=== {'NEW GAME':^47} ===\n')
